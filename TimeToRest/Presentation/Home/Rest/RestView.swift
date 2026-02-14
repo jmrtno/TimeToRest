559f700150ced1dcbd3b6d2df8c73f954d5589af
JaviDev <javi.apps.developer@gmail.com>
2026-02-14 02:13:54 +0100

    Identation

--- TimeToRest/Presentation/Home/Rest/RestView.swift
+++ TimeToRest/Presentation/Home/Rest/RestView.swift
@@ -145,10 +145,16 @@ struct RestView: View {
     private var infoCardsSection: some View {
         HStack(spacing: 16) {
             // Modo Estricto Card
-            GlassCard(icon: "shield", title: "Strict mode", value: viewModel.isStrictMode ? "Activated" : "Deactivated", iconColor: .indigo)
+            GlassCard(icon: "shield",
+                      title: "Strict mode",
+                      value: viewModel.isStrictMode ? "Activated" : "Deactivated",
+                      iconColor: .indigo)
             
             // Horario Card
-            GlassCard(icon: "clock", title: "Schedule", value: "\(viewModel.formattedStartTime) - \(viewModel.formattedEndTime)", iconColor: .gray)
+            GlassCard(icon: "clock",
+                      title: "Schedule",
+                      value: "\(viewModel.formattedStartTime) - \(viewModel.formattedEndTime)",
+                      iconColor: .gray)
         }
     }
 }
